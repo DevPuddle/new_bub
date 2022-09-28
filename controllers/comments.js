cbf0077c7197f40a8a372547ee7fc88df5bd901f
Isaiah Henson <Devpuddles@gmail.com>
2022-09-28 13:52:10 -0400

    added content to the controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,10 +3,13 @@ const Comment = require("../models/Comment");
 module.exports = {
   createComment: async (req, res) => {
     try {
+      // const commentUser = await User.findById(req.params.id)
       await Comment.create({
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
+        createdBy: req.user.userName,
+        createdById: req.user.id,
       });
       console.log("Comment has been added!");
       res.redirect("/post/"+req.params.id);
